45c769a7a9f6f35d58c50b4cca73163803f9e82c
Golnaz15 <gsarram@memphis.edu>
2015-02-23 11:20:48 -0600

    Check for indentation
    I edited the code by "Format document" option. Let's see if it works.

--- golnaz.cpp
+++ golnaz.cpp
@@ -1,21 +1,21 @@
 /*
- * Program Tools Course - HW4
- * by Golnaz Sarram
- * February 22th, 2015
- 
- * This progrem checks the access into the necessary computer resources
- */
+* Program Tools Course - HW4
+* by Golnaz Sarram
+* February 22th, 2015
+
+* This progrem checks the access into the necessary computer resources
+*/
 
 #include <iostream>
 using namespace std;
 
 int main () {
-    cout << "\n"
-	        "I was able to compile this code using the HPC at the University of Memphis. When I compiled it there, "
-	        "it did not produce any warning message. The HPC uses a GNU C++ compiler that can be considered a good "
-			"up-to-date standard. I also version-controlled this code using git, and used a remote repository hosted "
-			"by github. If I can do this, so can you!!!";
+	cout << "\n"
+		"I was able to compile this code using the HPC at the University of Memphis. When I compiled it there, "
+		"it did not produce any warning message. The HPC uses a GNU C++ compiler that can be considered a good "
+		"up-to-date standard. I also version-controlled this code using git, and used a remote repository hosted "
+		"by github. If I can do this, so can you!!!";
 	cout << endl;
-	
+
 	return 0;
 }
